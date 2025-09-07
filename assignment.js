0dea559a651e3fa7e3cb74435fd8e4642684f5a9
Mojolaoluwa <fadahunsi2018@yahoo.com>
2025-09-07 16:23:00 +0100

    Added assignment files

---
+++ assignment.js
@@ -0,0 +1,28 @@
+
+
+
+
+/*Print a message:
+"Hello [name], the result of num1 + num2 is [result]"
+push to github and enter the repository link*/
+//Declare three variables: num1, num2, and name. 
+//Assign number values to num1 and num2, and a string value to name
+let num1 = 10;
+let num2 = 3;
+let name = 'Mojolaoluwa';
+//Addition (num1 + num2) 
+let sum = num1 + num2;
+console.log(sum);
+//Subtraction (num1 - num2)
+let minus = num1 - num2;
+console.log(minus);
+//Multiplication (num1 * num2)
+let times = num1 * num2;
+console.log(times);
+//Division (num1 / num2) 
+let divide = num1/num2;
+console.log(divide);
+//Print a message:
+message = `Hello ${name}, the result of ${num1} + ${num2} is ${sum}`;
+console.log(message);
+alert(message);
